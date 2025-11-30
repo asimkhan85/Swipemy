7d263590197429b8749295d99c04ec4a6583afa8
asimkhan85 <asimkhan83437@gmail.com>
2025-11-30 11:09:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,27 @@
-let images = [
-    "images/image1.png",
-    "images/image2.png",
-    "images/image3.png",
-    "images/image4.png",
-    "images/image5.png",
-    "images/image6.png"
-];
-
-let current = 0;
-let img = document.getElementById("productImage");
+let img = document.getElementById("product");
+let total = 6;
+let current = 1;
 
 let startX = 0;
 
-document.addEventListener("touchstart", e => {
+img.addEventListener("touchstart", (e) => {
     startX = e.touches[0].clientX;
 });
 
-document.addEventListener("touchend", e => {
-    let endX = e.changedTouches[0].clientX;
+img.addEventListener("touchmove", (e) => {
+    let moveX = e.touches[0].clientX;
 
-    if (endX < startX - 50) {
-        current = (current + 1) % images.length; // next
+    if (moveX - startX > 20) {
+        current--;
+        if (current < 1) current = total;
+        img.src = "images/" + current + ".png";
+        startX = moveX;
     }
 
-    if (endX > startX + 50) {
-        current = (current - 1 + images.length) % images.length; // prev
+    if (startX - moveX > 20) {
+        current++;
+        if (current > total) current = 1;
+        img.src = "images/" + current + ".png";
+        startX = moveX;
     }
-
-    img.src = images[current];
 });
